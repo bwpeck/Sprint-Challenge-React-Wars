0c4459910921e450914a4032343c41987c98c9d5
bwpeck <benjaminwade483@yahoo.com>
2019-05-10 14:01:17 -0400

    finally got it working

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './StarWars.css'
+
+const Character = props => {
+  return(
+      <div className="characterCard">
+          <h2>{props.Character.name}</h2>
+          <p>{`Gender: ${props.Character.gender}`}</p>
+          <p>{`Birth Year: ${props.Character.birth_year}`}</p>
+          <p>{`Height: ${props.Character.height}`}</p>
+          <p>{`Mass: ${props.Character.mass}`}</p>
+          <p>{`Skin Color: ${props.Character.skin_color}`}</p>
+          <p>{`Eye Color: ${props.Character.eye_color}`}</p>
+          <p>{`Hair Color: ${props.Character.hair_color}`}</p>
+      </div>
+
+  );
+};
+
+export default Character;
